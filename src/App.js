8b4b7f853c5b338caf9cd51deb6baa0d4372e654
tarix <tarix@zedat.fu-berlin.de>
2020-01-30 18:10:10 +0100

    add Dashboard

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
 import ProTip from './ProTip';
+import Dashboard from "./Dashboard";
 
 function Copyright() {
     return (
@@ -20,14 +21,11 @@ function Copyright() {
 
 export default function App() {
     return (
-        <Container maxWidth="sm">
-            <Box my={4}>
-                <Typography variant="h4" component="h1" gutterBottom>
-                    Create React App v4-beta example
-                </Typography>
-                <ProTip />
-                <Copyright />
-            </Box>
-        </Container>
+        <div >
+            <Dashboard/>
+{/*
+            <Copyright />
+*/}
+        </div>
     );
 }

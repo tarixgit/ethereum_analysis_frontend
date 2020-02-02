86d8e8512f744215a557ac19b3b342ab6d3fba8a
tarix <tarix@zedat.fu-berlin.de>
2020-02-02 23:43:15 +0100

    - working version

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,92 +1,13 @@
 import React from 'react'
-import clsx from 'clsx'
 import { makeStyles } from '@material-ui/core/styles'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import Drawer from '@material-ui/core/Drawer'
-import Box from '@material-ui/core/Box'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import List from '@material-ui/core/List'
-import Typography from '@material-ui/core/Typography'
-import Divider from '@material-ui/core/Divider'
-import IconButton from '@material-ui/core/IconButton'
-import Badge from '@material-ui/core/Badge'
 import Container from '@material-ui/core/Container'
-import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper'
-import Link from '@material-ui/core/Link'
-import MenuIcon from '@material-ui/icons/Menu'
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
-import NotificationsIcon from '@material-ui/icons/Notifications'
-import { mainListItems, secondaryListItems } from './listItems'
-import Chart from './Chart'
-import Deposits from './Deposits'
-import Orders from './Orders'
-import Test from './Test'
-import TextField from '@material-ui/core/TextField'
-import Network from './Network'
 import CustomAppBar from './CustomAppBar'
 import LeftPanel from './LeftPanel'
-
-const drawerWidth = 240
+import EthereumGraph from './EthereumGraph'
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
-  },
-  toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
-  },
-  toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
-  title: {
-    flexGrow: 1,
-  },
-  drawerPaper: {
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
-    },
   },
   appBarSpacer: theme.mixins.toolbar,
   content: {
@@ -103,9 +24,6 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     overflow: 'auto',
     flexDirection: 'column',
-  },
-  fixedHeight: {
-    height: 240,
   },
 }))
 
@@ -130,7 +48,7 @@ export default function Dashboard() {
           className={classes.container}
           style={{ height: '100%' }}
         >
-          <Network />
+          <EthereumGraph />
         </Container>
       </main>
     </div>
--- src/EthereumGraph.jsx
+++ src/EthereumGraph.jsx
@@ -1,49 +1,136 @@
-import React, {useState} from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import { gql } from 'apollo-boost';
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import TextField from "@material-ui/core/TextField";
-import Deposits from "./Deposits";
-import Orders from "./Orders";
+import React, { useCallback, useState } from 'react'
+import { useLazyQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import TextField from '@material-ui/core/TextField'
+import Network from './Network'
+import { forEach, get, uniq, uniqBy } from 'lodash'
 
 const TRANSACTION = gql`
-query Address(address: String!) {
-  labels() {
-    id
-    name
-    color
+  query MyQuery($address: String!) {
+    addresses(address: $address) {
+      id
+      scam
+      hash
+      degree
+      alias
+      label {
+        color
+        name
+        id
+      }
+      transactionsInput {
+        amount
+        bid
+        id
+        fromAddress {
+          id
+          hash
+          scam
+          label {
+            color
+            name
+            id
+          }
+          degree
+          outdegree
+          alias
+        }
+      }
+      transactionsOutput {
+        amount
+        bid
+        id
+        toAddress {
+          id
+          hash
+          scam
+          label {
+            color
+            name
+            id
+          }
+          degree
+          outdegree
+          alias
+        }
+      }
+    }
   }
-}
+`
 
-`;
+const EthereumGraph = classes => {
+  let edges = []
+  let nodes = []
+  const [address, setAddress] = useState('')
+  const [loadNetworkData, { loading, error, data }] = useLazyQuery(TRANSACTION)
+  const changeAddress = useCallback(e => {
+    const { value } = e.target
+    if (value.length >= 42) {
+      loadNetworkData({
+        variables: { address: value.toLowerCase() },
+      })
+    }
+    setAddress(value)
+  })
 
-const EthereumGraph = (classes) => {
-  const [address, setAddress] = useState("")
-  const { loading, error, data } = useQuery(TRANSACTION, {variables: {address}});
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  return <Grid container spacing={3}>
-    <Grid item xs={12}>
-      <Paper className={classes.paper}>
-        <TextField
-          id="address-input"
-          label="Address"
-          autoFocus
-          fullWidth
-          value={address}
-          onChange={setAddress}
-        />
-      </Paper>
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+  const addressesWithIndo = get(data, 'addresses[0]', null)
+  if (addressesWithIndo) {
+    const mainAddressId = Number(addressesWithIndo.id)
+    const {
+      id,
+      hash,
+      alias,
+      label: { id: labelId },
+      transactionsInput,
+      transactionsOutput,
+    } = addressesWithIndo
+    nodes = [{ id: Number(id), label: alias || hash, group: labelId }]
+    edges = []
+    forEach(transactionsInput, ({ fromAddress }) => {
+      const {
+        id,
+        hash,
+        alias,
+        label: { id: labelId },
+      } = fromAddress
+      nodes.push({ id: Number(id), label: alias || hash, group: labelId })
+      edges.push({ from: Number(id), to: mainAddressId })
+    })
+    forEach(transactionsOutput, ({ toAddress }) => {
+      const {
+        id,
+        hash,
+        alias,
+        label: { id: labelId },
+      } = toAddress
+      nodes.push({ id: Number(id), label: alias || hash, group: labelId })
+      edges.push({ from: mainAddressId, to: Number(id) })
+    })
+  }
+  nodes = uniqBy(nodes, 'id')
+  return (
+    <Grid container spacing={3} style={{ height: '100%' }}>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>
+          <TextField
+            id="address-input"
+            label="Address"
+            autoFocus
+            fullWidth
+            value={address}
+            onChange={changeAddress}
+          />
+        </Paper>
+      </Grid>
+      <Grid item xs={12} style={{ height: '100%' }}>
+        <Network nodes={nodes} edges={edges} />
+      </Grid>
     </Grid>
-    <Grid item xs={12}>
-      <Paper className={classes.paper}>
-      </Paper>
-    </Grid>
-
-  </Grid>
+  )
 }
 
 export default EthereumGraph

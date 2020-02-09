36ec06a58fce60495f511f624ae79c39b3dfead8
tarix <tarix2006@gmail.com>
2020-02-09 01:21:07 +0100

    - test

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -29,7 +29,7 @@ const useStyles = makeStyles(theme => ({
 
 export default function Dashboard() {
   const classes = useStyles()
-  /*  const [open, setOpen] = React.useState(true)
+  /*   const [open, setOpen] = React.useState(true)
   const handleDrawerOpen = () => {
     setOpen(true)
   }
